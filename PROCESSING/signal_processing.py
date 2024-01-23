various causal algorithms for lift max id * polls stillness to wait for euler avg calcs * generalized helper functions * range offset fix working, in arch

PROCESSING/signal_processing.py
@@ -19,7 +19,7 @@ def diff(x1, x2):
     return [x1[i] - x2[i] for i in range(len(x1))]
 
 
-def fixZeroCrossings(x, c=0, single_scale=180):
+def makeContinuousRange(x, m=0.5, full_scale=360):
     """Fixes the zero crossings in a signal that is previously clamped to a 
     single scale range of `single_scale`.
 
@@ -28,50 +28,60 @@ def fixZeroCrossings(x, c=0, single_scale=180):
 
     This is useful for graphs when trying to test for stillness, since a zero crossing would be a massive
     outlier.
-    """
-    # y = x
-    # for i in range(len(x)):
-    #     if i == 0: continue
-    #     if x[i] > 0 and x[i] - x[i - 1] >= 1.85 * single_scale:
-    #         # sub cm from xi and all following el until the opposite crossing
-    #         for j in range(len(x) - i):
-    #             y[i + j] = x[i + j] - 2 * single_scale
-    #             if x[i] < 0 and x[i] - x[i - 1] <= -1.85 * single_scale:
-    #                 break
-    #         continue
 
-    #     if x[i] < 0 and x[i] - x[i - 1] <= -1.85 * single_scale:
-    #         # sub cm from xi and all following el until the opposite crossing
-    #         for j in range(len(x) - i):
-    #             y[i + j] = x[i + j] + 2 * single_scale
-    #             if x[i] > 0 and x[i] - x[i - 1] >= 1.85 * single_scale:
-    #                 break
-    #         continue
+    Returns
+    -------
+    unclamped signal `x` and the observed skipping indices
+    """
+    y = x
+    skips = []
+    for i in range(len(x)):
+        if i == 0: continue
+        if x[i] > x[i - 1] and abs(x[i] - x[i - 1]) >= (m * full_scale):
+            skips.append([i - 1, i])
+            for j in range(len(x) - i):
+                y[i + j] = x[i + j] - full_scale
+                if x[i] < x[i - 1] and abs(x[i] - x[i - 1]) >= (m * full_scale):
+                    skips.append([i - 1, i])
+                    break
+            continue
+
+        if x[i] < x[i - 1] and abs(x[i] - x[i - 1]) >= (m * full_scale):
+            skips.append([i - 1, i])
+            for j in range(len(x) - i):
+                y[i + j] = x[i + j] + full_scale
+                if x[i] > x[i - 1] and abs(x[i] - x[i - 1]) >= (m * full_scale):
+                    skips.append([i - 1, i])
+                    break
+            continue
+    return y, skips
+
+    # print(len(x))
+    # m = 0.95
+    # y = x
+    # for i in range(len(x) - 1):
+    #     # if i == 0: continue
+    #     y[i] = x[i] + c
+    #     if x[i + 1] > 0 and (x[i + 1] - x[i]) >= (m * full_scale):
+    #         fixZeroCrossings(x[i + 1:], -full_scale)
+
+    #     if x[i + 1] < 0 and (x[i + 1] - x[i]) <= -(m * full_scale):
+    #         fixZeroCrossings(x[i + 1:], full_scale)
     # return y
 
-    print(len(x))
 
-    y = x
-    for i in range(len(x) - 1):
-        # if i == 0: continue
-        if x[i + 1] > 0 and (x[i + 1] - x[i]) >= (1.85 * single_scale):
-            fixZeroCrossings(x[i + 1:], -2 * single_scale)
-
-        if x[i + 1] < 0 and (x[i + 1] - x[i]) <= (-1.85 * single_scale):
-            fixZeroCrossings(x[i + 1:], 2 * single_scale)
-        y[i] = x[i] + c
-    return y
-
-
-def fixZeroCrossings3dof(x):
+def makeContinuousRange3dof(x, fix_0=True, fix_1=True, fix_2=True):
     """Runs `fixZeroCrossing()` for each column signal in the input ndarray `x`
     
     Returns the ndarray, with fixed zero crossings on each column signal individually.
     """
+    rollFix, roll_skips = makeContinuousRange(x[:, 0].tolist(), full_scale=180)
+    pitchFix, pitch_skips = makeContinuousRange(x[:, 1].tolist(), full_scale=180)
+    yawFix, yaw_skips = makeContinuousRange(x[:, 2].tolist())
     return np.transpose([
-        x[:, 0],#np.array(fixZeroCrossings(x[:, 0].tolist())),
-        x[:, 1],#np.array(fixZeroCrossings(x[:, 1].tolist())),
-        np.array(fixZeroCrossings(x[:, 2].tolist())),
+        np.array(rollFix) if len(roll_skips) > 0 and fix_0 is True else x[:, 0],
+        np.array(pitchFix) if len(pitch_skips) > 0 and fix_1 is True else x[:, 1],
+        np.array(yawFix) if len(yaw_skips) > 0 and fix_2 is True else x[:, 2],
     ])
 
 

PROCESSING/tile.py
@@ -1,5 +1,5 @@
 from jump import Jump
-from signal_processing import fixZeroCrossings3dof, length, lowpass
+from signal_processing import makeContinuousRange3dof, length, lowpass
 import imufusion
 import numpy as np
 
@@ -16,6 +16,9 @@ class Tile:
             corrected_alt: list[float],
             euler6=None,
             euler9=None,
+            continuous_roll=False,
+            continuous_pitch=False,
+            continuous_yaw=True,
     ):
         self.time = time
         self.ax = ax
@@ -34,16 +37,22 @@ class Tile:
         # set from the sync, using the ground truth to account constant offsets
         self.corrected_alt = corrected_alt
 
-        # set imu signals, allowing override from parent
-        self.euler6 = self.computeEuler6() if euler6 is None else euler6
-        self.euler9 = self.computeEuler9() if euler9 is None else euler9
-        # self.euler6 = fixZeroCrossings3dof(self.euler6)
-        # self.euler9 = fixZeroCrossings3dof(self.euler9)
+        # set imu signals with corrections if requested, unless signal is overrides from init
+        self.euler6 = makeContinuousRange3dof(
+            self.computeEuler6(),
+            fix_0=continuous_roll,
+            fix_1=continuous_pitch,
+            fix_2=continuous_yaw) if euler6 is None else euler6
+        self.euler9 = makeContinuousRange3dof(
+            self.computeEuler9(),
+            fix_0=continuous_roll,
+            fix_1=continuous_pitch,
+            fix_2=continuous_yaw) if euler9 is None else euler9
 
-        self.euler6_norm = [np.linalg.norm(self.euler6[row, :]) for row in range(self.euler6.shape[0])]
-        self.euler9_norm = [np.linalg.norm(self.euler9[row, :]) for row in range(self.euler9.shape[0])]
-        # self.euler6_length = length(self.euler6[:, 0], self.euler6[:, 1], self.euler6[:, 2])
-        # self.euler9_length = length(self.euler9[:, 0], self.euler9[:, 1], self.euler9[:, 2])
+        # self.euler6_norm = [np.linalg.norm(self.euler6[row, :]) for row in range(self.euler6.shape[0])]
+        # self.euler9_norm = [np.linalg.norm(self.euler9[row, :]) for row in range(self.euler9.shape[0])]
+        self.euler6_length = length(self.euler6[:, 0], self.euler6[:, 1], self.euler6[:, 2])
+        self.euler9_length = length(self.euler9[:, 0], self.euler9[:, 1], self.euler9[:, 2])
 
 
     def __printProps__(self, prefix="\t"):